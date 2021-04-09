1f96089572d04f178bbf314078f972ee713d515a
Ayokunmi <ayokunmiolusa@gmail.com>
2021-04-09 16:38:50 +0100

    Added js

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
+
+//Start the game    
+
+const match = document.querySelector('.match');
+
+// const restartBtn = document.querySelector('.restart');
+    const restart = () => {
+            location.reload();
+        
+    }
+
+
 const game = ()=> {
     let pScore = 0;
     let cScore = 0;
 
-    //Start the game    
-    const startGame = ()=> {
+
+    const startGame = () =>{
         const playBtn = document.querySelector('.intro button');
         const introScreen = document.querySelector('.intro');
-        const match = document.querySelector('.match');
-
+    
         playBtn.addEventListener('click', ()=> {
             introScreen.classList.add('fadeOut');
             match.classList.add('fadeIn');
-
-        });
+        });    
     };
+
     //Play match
     const playMatch = ()=> {
         const options = document.querySelectorAll('.options button');
@@ -37,11 +48,13 @@ const game = ()=> {
                 //How the computer compares hands
                 
                setTimeout(() =>{
-                compareHands(this.textContent,computerChoice)
 
                 // Update Images
                 playerHand.src = `./images/${this.textContent}.png`;
                 computerHand.src = `./images/${computerChoice}.png`;
+
+                compareHands(this.textContent,computerChoice)
+                console.log(this.textContent,computerChoice)
                },1500 )
 
                 playerHand.style.animation = "shakePlayer 1.5s ease";
@@ -52,63 +65,71 @@ const game = ()=> {
         });
     };    
     
-    const updateScore = () =>{
-        const playerScore = document.querySelector('.player-score p');
-        const computerScore = document.querySelector('.computer-score p');  
-        playerScore.textContent = pScore;
-        computerScore.textContent = cScore;
-    };      
+   
     
-        
-    const compareHands = (playerChoice, computerChoice) =>{
-        const winner = document.querySelector('.winner');
-        if(playerChoice === computerChoice){
-            winner.textContent = 'It is a tie.';
-            return;
-        }
-
-        if (playerChoice === 'rock'){
-            if(computerChoice === 'scissors'){
-                winner.textContent = 'Player wins!';
-                pScore++;
-                updateScore();
-                return;
-            }else{
-                winner.textContent = 'Computer wins!'
-                cScore++;
-                updateScore();
-                return;
-            }
-        }
-        if (playerChoice === 'paper'){
-            if(computerChoice === 'rock'){
-                winner.textContent = 'Player wins!'
-                pScore++;
-                updateScore();
-                return;
-            }else{
-                winner.textContent = 'Computer wins!'
-                cScore++;
-                updateScore();
-                return;
-            }
-        }
-        if (playerChoice === 'scissors'){
-            if(computerChoice === 'paper'){
-                winner.textContent = 'Player wins!'
-                pScore++;
-                updateScore();
-                return;
-            }else{
-                winner.textContent = 'Computer wins!'
-                cScore++;
-                updateScore();
-                return;
-            }
-        }
-
+const updateScore = () => {
+    const playerScore = document.querySelector('.player-score p');
+    const computerScore = document.querySelector('.computer-score p');
+    const limit = document.querySelector('.limit p').textContent;
+    const outro = document.querySelector('.outro');
+    // console.log(limit, parseInt(limit));
+    playerScore.textContent = pScore;
+    computerScore.textContent = cScore;
+    if (parseInt(limit) === pScore) {
+        // alert('Player Wins!!');
+        match.classList.remove('fadeIn');
+        match.classList.add('fadeOut');
+        outro.classList.remove('fadeOut');
+        outro.classList.add('fadeIn');
+        outro.style.animation = "1.5s ease"
+        document.querySelector('.outro').innerHTML = `<h1> Game Over, Player Wins!!!</h1>
+        <button onclick="restart()">Play Again</button>`;
+    } else if (parseInt(limit) === cScore) {
+        // alert('Computer Wins!!');
+        match.classList.remove('fadeIn');
+        match.classList.add('fadeOut');
+        outro.classList.remove('fadeOut');
+        outro.classList.add('fadeIn');
+        document.querySelector('.outro').innerHTML = `<h1> Game Over, Computer Wins!!!</h1>
+        <button onclick="restart()">Play Again</button>`;
     }
+};      
 
+    
+const compareHands = (playerChoice, computerChoice) =>{
+    const winner = document.querySelector('.winner');
+    if(playerChoice === computerChoice){
+        winner.textContent = 'It is a tie.';
+        console.log(pScore, cScore);
+        return;
+    } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
+        winner.textContent = 'Player wins this round!';
+        pScore++;
+        console.log(pScore, cScore);
+        updateScore();
+        return;
+    } else if (playerChoice === 'paper' && computerChoice === 'rock') {
+        winner.textContent = 'Player wins this round!';
+        pScore++;
+        console.log(pScore, cScore);
+        updateScore();
+        return;
+    } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
+        winner.textContent = 'Player wins this round!';
+        pScore++;
+        console.log(pScore, cScore);
+        updateScore();
+        return;
+    } else{
+        winner.textContent = 'Computer wins this round!'
+        cScore++;
+        console.log(pScore, cScore);
+        updateScore();
+        return;
+    }
+}
+
+    
     //Call all the inner functions
     startGame();
     playMatch();
@@ -116,6 +137,4 @@ const game = ()=> {
 
 
 };
-
 game();
-
